6560f1763280ccf0b9058287c5a146d384ebf7ba
Gil Tomás <gil.tms@gmail.com>
2023-03-18 18:02:26 +0000

    Move ‘time’ segment to after ‘misc-info’ segment

--- doom-modeline.el
+++ doom-modeline.el
@@ -89,51 +89,51 @@
 
 (doom-modeline-def-modeline 'main
   '(bar workspace-name window-number modals matches follow buffer-info remote-host buffer-position word-count parrot selection-info)
-  '(compilation objed-state misc-info persp-name battery grip irc mu4e gnus github debug repl lsp minor-modes input-method indent-info buffer-encoding process vcs checker time major-mode))
+  '(compilation objed-state misc-info time persp-name battery grip irc mu4e gnus github debug repl lsp minor-modes input-method indent-info buffer-encoding process vcs checker major-mode))
 
 (doom-modeline-def-modeline 'minimal
   '(bar matches buffer-info-simple)
-  '(media-info time major-mode))
+  '(media-info major-mode))
 
 (doom-modeline-def-modeline 'special
   '(bar window-number modals matches buffer-info remote-host buffer-position word-count parrot selection-info)
-  '(compilation objed-state misc-info battery irc-buffers debug minor-modes input-method indent-info buffer-encoding process time major-mode))
+  '(compilation objed-state misc-info time battery irc-buffers debug minor-modes input-method indent-info buffer-encoding process major-mode))
 
 (doom-modeline-def-modeline 'project
   '(bar window-number modals buffer-default-directory remote-host buffer-position)
-  '(compilation misc-info battery irc mu4e gnus github debug minor-modes input-method process time major-mode))
+  '(compilation misc-info time battery irc mu4e gnus github debug minor-modes input-method process major-mode))
 
 (doom-modeline-def-modeline 'dashboard
   '(bar window-number buffer-default-directory-simple remote-host)
-  '(compilation misc-info battery irc mu4e gnus github debug minor-modes input-method process time major-mode))
+  '(compilation misc-info time battery irc mu4e gnus github debug minor-modes input-method process major-mode))
 
 (doom-modeline-def-modeline 'vcs
   '(bar window-number modals matches buffer-info remote-host buffer-position parrot selection-info)
-  '(compilation misc-info battery irc mu4e gnus github debug minor-modes buffer-encoding process time major-mode))
+  '(compilation misc-info time battery irc mu4e gnus github debug minor-modes buffer-encoding process major-mode))
 
 (doom-modeline-def-modeline 'package
   '(bar window-number package)
-  '(compilation misc-info process time major-mode))
+  '(compilation misc-info time process major-mode))
 
 (doom-modeline-def-modeline 'info
   '(bar window-number buffer-info info-nodes buffer-position parrot selection-info)
-  '(compilation misc-info buffer-encoding time major-mode))
+  '(compilation misc-info time buffer-encoding major-mode))
 
 (doom-modeline-def-modeline 'media
   '(bar window-number buffer-size buffer-info)
-  '(compilation misc-info media-info process vcs time major-mode))
+  '(compilation misc-info time media-info process vcs major-mode))
 
 (doom-modeline-def-modeline 'message
   '(bar window-number modals matches buffer-info-simple buffer-position word-count parrot selection-info)
-  '(compilation objed-state misc-info battery debug minor-modes input-method indent-info buffer-encoding time major-mode))
+  '(compilation objed-state misc-info time battery debug minor-modes input-method indent-info buffer-encoding major-mode))
 
 (doom-modeline-def-modeline 'pdf
   '(bar window-number matches buffer-info pdf-pages)
-  '(compilation misc-info process vcs time major-mode))
+  '(compilation misc-info time process vcs major-mode))
 
 (doom-modeline-def-modeline 'org-src
   '(bar window-number modals matches buffer-info buffer-position word-count parrot selection-info)
-  '(compilation objed-state misc-info debug lsp minor-modes input-method indent-info buffer-encoding process checker time major-mode))
+  '(compilation objed-state misc-info time debug lsp minor-modes input-method indent-info buffer-encoding process checker major-mode))
 
 (doom-modeline-def-modeline 'helm
   '(bar helm-buffer-id helm-number helm-follow helm-prefix-argument)
@@ -141,11 +141,11 @@
 
 (doom-modeline-def-modeline 'timemachine
   '(bar window-number modals matches git-timemachine buffer-position word-count parrot selection-info)
-  '(misc-info minor-modes indent-info buffer-encoding time major-mode))
+  '(misc-info time minor-modes indent-info buffer-encoding major-mode))
 
 (doom-modeline-def-modeline 'calculator
   '(window-number modals matches calc buffer-position)
-  '(misc-info minor-modes process major-mode))
+  '(misc-info time minor-modes process major-mode))
 
 
 ;;
